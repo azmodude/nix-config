7e51a75e206a095266e4f873837792d35e9e40ee
Gordon Schulz <gordon@gordonschulz.de>
2023-02-26 13:59:57 +0100

    Setup atuin

--- home/azmo/features/cli/atuin.nix
+++ home/azmo/features/cli/atuin.nix
@@ -1,5 +1,19 @@
+{ config, ...}:
 {
   programs.atuin = {
     enable = true;
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      filter_mode = "global";
+      filter_mode_shell_up_key_binding = "host";
+      update_check = "false";
+    };
+  };
+  sops = {
+    defaultSopsFile = ../../secrets.yaml;
+    secrets.atuin-key = {
+      path = "${config.xdg.dataHome}/atuin/key";
+    };
   };
 }
