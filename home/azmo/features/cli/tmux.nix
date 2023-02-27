faecd449b806a0f4f329044c7ec06a5e515efc34
Gordon Schulz <gordon@gordonschulz.de>
2023-02-27 08:39:18 +0100

    Add catppuccin theme to tmux

--- home/azmo/features/cli/tmux.nix
+++ home/azmo/features/cli/tmux.nix
@@ -1,4 +1,21 @@
-{pkgs, ...}: {
+{pkgs, ...}: let
+  catppuccin = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "catppuccin";
+    version = "unstable-2022-12-14";
+    src = pkgs.fetchFromGitHub {
+      owner = "catppuccin";
+      repo = "tmux";
+      rev = "e2561de";
+      sha256 = "sha256:6UmFGkUDoIe8k+FrzdzsKrDHHMNfkjAk0yyc+HV199M=";
+    };
+    postInstall = ''
+      sed -i -e 's|sed|${pkgs.gnused}/bin/sed|g;s|/bin/cat|${pkgs.coreutils}/bin/cat|g' $target/catppuccin.tmux
+      # catppuccing tries to write to $target, patch that to point to /tmp instead
+      sed -i -r "36s/\\$\{PLUGIN_DIR\}/\/tmp/g" $target/catppuccin.tmux
+      sed -i -r "39s/\\$\{PLUGIN_DIR\}/\/tmp/g" $target/catppuccin.tmux
+    '';
+  };
+in {
   programs.tmux = {
     enable = true;
     customPaneNavigationAndResize = true;
@@ -12,6 +29,13 @@
       }
       {
         plugin = tmuxPlugins.pain-control;
+      }
+      {
+        plugin = catppuccin;
+        extraConfig = ''
+          set -g @catppuccin_flavour 'macchiato';
+          set -g @catppuccin_window_tabs_enabled on
+        '';
       }
     ];
   };
