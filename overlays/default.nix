a72d0528a3020f6b0de64ed06e158a87ee2d9a8d
Gordon Schulz <gordon@gordonschulz.de>
2023-03-01 19:04:39 +0100

    Add vivid overlay and add to zshrc

--- overlays/default.nix
+++ overlays/default.nix
@@ -7,6 +7,24 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
+    # override vivid until new version is cut.
+    # catppuccin-theme is only in recent git commits
+    vivid = prev.vivid.overrideAttrs (oldAttrs: rec {
+      pname = "vivid";
+      version = "0.8.0-4d7fb24";
+      src = prev.fetchFromGitHub {
+        owner = "sharkdp";
+        repo = "${pname}";
+        rev = "4d7fb24";
+        sha256 = "sha256:rfEkeNrC5LHmUnTGv3tpIzpF+aSDCNmsnNsBQ/GC3Q4=";
+      };
+      cargoDeps = oldAttrs.cargoDeps.overrideAttrs (prev.lib.const {
+        name = "${pname}-vendor.tar.gz";
+        inherit src;
+        outputHash = "sha256-P+Mecvhm5vXvbZLT/mDEBsfB/hoUfaaTg6glD+5HznY=";
+      });
+
+      });
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
