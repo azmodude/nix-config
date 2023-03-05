5bcc338d025b2e69a27b5a2e2035595e5208e95c
Gordon Schulz <gordon@gordonschulz.de>
2023-03-05 12:02:06 +0100

    WIP: add artemis home-manager nix

---
+++ home/azmo/artemis.nix
@@ -0,0 +1,53 @@
+{
+  inputs,
+  pkgs,
+  pkgs-stable,
+  ...
+}: {
+  imports = [
+    ./global
+    ./features/cli/atuin.nix
+    ./features/cli/direnv.nix
+    ./features/cli/fzf.nix
+    ./features/cli/gpg.nix
+    ./features/cli/git.nix
+    ./features/cli/ssh.nix
+    ./features/cli/starship.nix
+    ./features/cli/tmux.nix
+    ./features/cli/zellij.nix
+    ./features/cli/zsh.nix
+    ./features/desktop/common/alacritty.nix
+    ./features/desktop/common/firefox.nix
+    ./features/desktop/common/fonts.nix
+    ./features/desktop/gnome
+    ./features/desktop/common/wezterm.nix
+    ./features/nvim
+    ./features/emacs
+    ./features/desktop/common/vscode.nix
+  ];
+  home.packages = with pkgs; [
+    bat
+    exa
+    google-chrome
+    intel-gpu-tools
+    just
+    ripgrep
+    vivaldi
+    vivaldi-ffmpeg-codecs
+  ];
+
+  programs.zoxide = {
+    enable = true;
+    enableZshIntegration = true;
+    enableBashIntegration = true;
+  };
+  sops = {
+    age.keyFile = "/home/azmo/.config/sops/age/keys.txt";
+    defaultSopsFile = ./secrets.yaml;
+    #    secrets.ssh-config-personal = {
+    #      path = "%r/ssh/ssh-config-personal";
+    secrets.ssh-config-personal = {
+      path = ".ssh/config.d/ssh-config-personal";
+    };
+  };
+}
