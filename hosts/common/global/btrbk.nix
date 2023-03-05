7ec063b9e3378d542227c10ce0fce9dbe981d7c5
Gordon Schulz <gordon@gordonschulz.de>
2023-03-05 17:02:57 +0100

    Snapshot /persist and keep snapshots around longer

--- hosts/common/global/btrbk.nix
+++ hosts/common/global/btrbk.nix
@@ -3,8 +3,8 @@
     instances = {
       btrbk.settings = {
         timestamp_format = "long";
-        snapshot_preserve_min = "24h";
-        snapshot_preserve = "48h";
+        snapshot_preserve_min = "2d";
+        snapshot_preserve = "7d";
 
         volume = {
           "/.btrfs-root" = {
@@ -12,6 +12,12 @@
               "@home" = {
                 snapshot_create = "always";
                 snapshot_dir = "@snapshots/home";
+              };
+            };
+            subvolume = {
+              "@persist" = {
+                snapshot_create = "always";
+                snapshot_dir = "@snapshots/persist";
               };
             };
           };
@@ -25,5 +31,6 @@
   # ensure snapshots_dir exists
   systemd.tmpfiles.rules = [
     "d /.btrfs-root/@snapshots/home 0755 root root - -"
+    "d /.btrfs-root/@snapshots/persist 0755 root root - -"
   ];
 }
