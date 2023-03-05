ec9d9f14626b06d18406317910c4fed76e9640e3
Gordon Schulz <gordon@gordonschulz.de>
2023-03-05 14:25:31 +0100

    Add missing curly bracket

--- hosts/common/optional/encrypted-root-ssh-unlock.nix
+++ hosts/common/optional/encrypted-root-ssh-unlock.nix
@@ -5,3 +5,4 @@ boot.initrd.network.ssh = {
   authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAFOj2KxBYTb6MclwoN4xgKN7OgX4Iwv4lViluKPdL9K" ];
   hostKeys = [ "/persist/etc/ssh/ssh_host_ed25519_key" "/persist/etc/ssh/ssh_host_rsa_key" ];
 };
+}
