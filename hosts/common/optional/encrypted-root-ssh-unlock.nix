5732129b17f5eeb0426fb78a053568ab480d771b
Gordon Schulz <gordon@gordonschulz.de>
2023-03-05 17:03:19 +0100

    Actually enable network for unlocking

--- hosts/common/optional/encrypted-root-ssh-unlock.nix
+++ hosts/common/optional/encrypted-root-ssh-unlock.nix
@@ -1,8 +1,14 @@
 {
-boot.initrd.network.ssh = {
-  enable = true;
-  port = 22;
-  authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAFOj2KxBYTb6MclwoN4xgKN7OgX4Iwv4lViluKPdL9K" ];
-  hostKeys = [ "/persist/etc/ssh/ssh_host_ed25519_key" "/persist/etc/ssh/ssh_host_rsa_key" ];
+boot.initrd = {
+  network = {
+    enable = true;
+    ssh = {
+      enable = true;
+      port = 22;
+      authorizedKeys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAFOj2KxBYTb6MclwoN4xgKN7OgX4Iwv4lViluKPdL9K" ];
+      hostKeys = [ "/persist/etc/ssh/ssh_host_ed25519_key" "/persist/etc/ssh/ssh_host_rsa_key" ];
+    };
+  };
+  availableKernelModules = [ "igc" ];
 };
 }
