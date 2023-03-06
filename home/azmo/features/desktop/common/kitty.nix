c37949c21104e7769c607b8baef96dd4da0cd23f
Gordon Schulz <gordon@gordonschulz.de>
2023-03-06 10:02:21 +0100

    nix fmt

--- home/azmo/features/desktop/common/kitty.nix
+++ home/azmo/features/desktop/common/kitty.nix
@@ -1,50 +1,48 @@
-{pkgs, ...}:
-let
- nvim-minimal = pkgs.writeTextFile {
-  name = "nvim-minimal.vim";
-  executable = false;
-  text = ''
-    set relativenumber
-    set number
-    set mouse=a
-    set clipboard+=unnamedplus
-    set virtualedit=all
-    set scrollback=100000
-    set termguicolors
-    set laststatus=0
-    set background=dark
-    set ignorecase
-    set scrolloff=8
+{pkgs, ...}: let
+  nvim-minimal = pkgs.writeTextFile {
+    name = "nvim-minimal.vim";
+    executable = false;
+    text = ''
+      set relativenumber
+      set number
+      set mouse=a
+      set clipboard+=unnamedplus
+      set virtualedit=all
+      set scrollback=100000
+      set termguicolors
+      set laststatus=0
+      set background=dark
+      set ignorecase
+      set scrolloff=8
 
-    map <silent> q :qa!<CR>
+      map <silent> q :qa!<CR>
 
-    " Short highlight on yanked text
-    augroup highlight_yank
-        autocmd!
-        autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 40})
-    augroup END
+      " Short highlight on yanked text
+      augroup highlight_yank
+          autocmd!
+          autocmd TextYankPost * silent! lua require'vim.highlight'.on_yank({timeout = 40})
+      augroup END
 
-    augroup start_at_bottom
-        autocmd!
-        autocmd VimEnter * normal G
-    augroup END
+      augroup start_at_bottom
+          autocmd!
+          autocmd VimEnter * normal G
+      augroup END
 
-    augroup prevent_insert
-        autocmd!
-        autocmd TermEnter * stopinsert
-    augroup END
-   '';
+      augroup prevent_insert
+          autocmd!
+          autocmd TermEnter * stopinsert
+      augroup END
+    '';
   };
-in
-{
-    programs.kitty = {
-     enable = true;
-     font = {
+in {
+  programs.kitty = {
+    enable = true;
+    font = {
       name = "Iosevka Medium";
       size = 11.0;
-     };
-     theme = "Catppuccin-Macchiato";
-     settings = {
+    };
+    theme = "Catppuccin-Macchiato";
+    settings = {
       modify_font = "underline_position 1";
       wayland_titlebar_color = "background";
       enable_audio_bell = "no";
@@ -63,6 +61,6 @@ in
       scrollback_lines = "10000";
       scrollback_pager = "${pkgs.neovim-nightly}/bin/nvim -u ${nvim-minimal} -c 'set signcolumn=no showtabline=0' -c 'silent write! /tmp/kitty_scrollback_buffer | te cat /tmp/kitty_scrollback_buffer - '";
       scrollback_pager_history_size = "20";
-     };
     };
+  };
 }
