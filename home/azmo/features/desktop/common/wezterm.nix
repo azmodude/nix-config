9cf3865588531f04c8ffc341f1d7577384931542
Gordon Schulz <gordon@gordonschulz.de>
2023-03-06 09:49:58 +0100

    Update wezterm configuration

--- home/azmo/features/desktop/common/wezterm.nix
+++ home/azmo/features/desktop/common/wezterm.nix
@@ -13,11 +13,23 @@
       return {
        -- ...your existing config
        color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
-        font = wezterm.font("Iosevka Nerd Font"),
-        font_size = 11.0,
+        font = wezterm.font_with_fallback(
+        {
+          "Iosevka",
+          "Symbols Nerd Font"
+        }),
+        font_size = 10.5,
         hide_tab_bar_if_only_one_tab = true,
+        xcursor_theme = "Adwaita",
+        window_frame = {
+         font = wezterm.font { family = "Inter", weight = "Bold" },
+         font_size = 10.0,
+        },
+        window_padding = {
+         left = 5,
+         right = 5,
+        },
       }
     '';
   };
-  home.sessionVariables.XCURSOR_THEME = "adwaita";
 }
