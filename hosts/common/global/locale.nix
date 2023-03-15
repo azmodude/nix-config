a67443a7047dfc56e4f9c6538fe0294a44d98595
Gordon Schulz <gordon@gordonschulz.de>
2023-03-15 12:27:36 +0100

    Complete locale

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -2,7 +2,15 @@
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_TIME = lib.mkDefault "de_DE.UTF-8";
+      LC_ADDRESS = "en_DE.utf8";
+      LC_IDENTIFICATION = "en_DE.utf8";
+      LC_MEASUREMENT = "en_DE.utf8";
+      LC_MONETARY = "en_DE.utf8";
+      LC_NAME = "en_DE.utf8";
+      LC_NUMERIC = "en_DE.utf8";
+      LC_PAPER = "en_DE.utf8";
+      LC_TELEPHONE = "en_DE.utf8";
+      LC_TIME = "en_DE.utf8";
     };
     supportedLocales = lib.mkDefault [
       "en_US.UTF-8/UTF-8"
