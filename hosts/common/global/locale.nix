c098ea01118f753034a371de137dbbe1270450c3
Gordon Schulz <gordon@gordonschulz.de>
2023-03-15 12:30:25 +0100

    Fix locale

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -2,15 +2,15 @@
   i18n = {
     defaultLocale = lib.mkDefault "en_US.UTF-8";
     extraLocaleSettings = {
-      LC_ADDRESS = "en_DE.utf8";
-      LC_IDENTIFICATION = "en_DE.utf8";
-      LC_MEASUREMENT = "en_DE.utf8";
-      LC_MONETARY = "en_DE.utf8";
-      LC_NAME = "en_DE.utf8";
-      LC_NUMERIC = "en_DE.utf8";
-      LC_PAPER = "en_DE.utf8";
-      LC_TELEPHONE = "en_DE.utf8";
-      LC_TIME = "en_DE.utf8";
+      LC_ADDRESS = "de_DE.UTF-8";
+      LC_IDENTIFICATION = "de_DE.UTF-8";
+      LC_MEASUREMENT = "de_DE.UTF-8";
+      LC_MONETARY = "de_DE.UTF-8";
+      LC_NAME = "de_DE.UTF-8";
+      LC_NUMERIC = "de_DE.UTF-8";
+      LC_PAPER = "de_DE.UTF-8";
+      LC_TELEPHONE = "de_DE.UTF-8";
+      LC_TIME = "de_DE.UTF-8";
     };
     supportedLocales = lib.mkDefault [
       "en_US.UTF-8/UTF-8"
