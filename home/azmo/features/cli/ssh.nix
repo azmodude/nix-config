938fb7d6f3c07dcb34f81fe6dbd35527e4421009
Gordon Schulz <gordon@gordonschulz.de>
2023-03-21 10:52:23 +0100

    chore: Update ssh-config and add public keys

--- home/azmo/features/cli/ssh.nix
+++ home/azmo/features/cli/ssh.nix
@@ -26,12 +26,21 @@ in {
         forwardAgent = true;
       };
     };
+    serverAliveInterval = 15;
+    hashKnownHosts = true;
   };
+
+  # write SSH public keys
+  home.file.".ssh/general.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUkEk7GV/qWMR9SJFYSJSxwnPxR8fG2Fn9VILHcyPYQ";
+  home.file.".ssh/git.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQh4341SBJaXdFAvDtAvS9zs4tiS5v8jod0OhgzBXpV";
+  home.file.".ssh/netcup.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGgxP6hdnBAK8gYgHBFwmtNvgQmFii0RUiSifZ3KgmlU";
+  home.file.".ssh/upcloud.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ6nxegS8y2rC6ldf4mxG3iyuJilxvDLzCote2RSmViG";
 
   # ensure secure controlPath exists and secure ~/.ssh
   systemd.user.tmpfiles.rules = [
     "d /home/azmo/tmp/.ssh 0700 azmo azmo 10d -"
     "z /home/azmo/.ssh 0700 azmo azmo - -"
+    "z /home/azmo/.ssh/*.pub 0600 azmo azmo - -"
   ];
 
   home.persistence."/persist/home/azmo" = {
