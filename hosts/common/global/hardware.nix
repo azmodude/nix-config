4931c14889966503cef35c7f349f4746bb3bcbe3
Gordon Schulz <gordon@gordonschulz.de>
2023-03-25 15:24:38 +0100

    Remove obsolete hidpi option

--- hosts/common/global/hardware.nix
+++ hosts/common/global/hardware.nix
@@ -1,6 +1,4 @@
 {lib, ...}: {
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = true;
-  # high-resolution display
-  hardware.video.hidpi.enable = lib.mkDefault true;
 }
