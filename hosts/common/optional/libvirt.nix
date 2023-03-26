9b6e8dcecf220a2145c08b873709cabe32c01a62
Gordon Schulz <gordon@gordonschulz.de>
2023-03-26 17:16:06 +0200

    Refine libvirt configuration

--- hosts/common/optional/libvirt.nix
+++ hosts/common/optional/libvirt.nix
@@ -1,9 +1,30 @@
+{ config, pkgs, lib, desktop, ...}:
 {
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
       swtpm.enable = true;
       ovmf.enable = true;
+      ovmf.packages = with pkgs; [ OVMFFull.fd ];
     };
   };
+
+  # add management packages if we have a desktop set
+  environment.systemPackages = [ ] ++ lib.lists.optionals (builtins.isString desktop) [pkgs.virt-manager pkgs.freerdp];
+
+  # easier access to most common ovmf files
+  environment.etc = {
+  "ovmf/edk2-x86_64-secure-code.fd" = {
+    source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+    mode = "0644";
+    user = "libvirtd";
+  };
+
+  "ovmf/edk2-i386-vars.fd" = {
+    source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
+    mode = "0644";
+    user = "libvirtd";
+  };
+};
+
 }
