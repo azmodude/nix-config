e36f07dc7b71daa254120ae6cf75d576f364e2bf
Gordon Schulz <gordon@gordonschulz.de>
2023-03-26 14:37:29 +0200

    Remove obsolete persistence
    It is handled by host's snycthing settings

--- home/azmo/features/syncthing.nix
+++ home/azmo/features/syncthing.nix
@@ -1,11 +1,6 @@
-{ config, ... }:
-{
+{config, ...}: {
   # make syncthing related links for workstations
   systemd.user.tmpfiles.rules = [
     "L+ ${config.home.homeDirectory}/Documents/synced - - - - ${config.home.homeDirectory}/sync/workstations/documents"
   ];
-
-  home.persistence."/persist/home/azmo" = {
-    directories = ["sync"];
-  };
 }
