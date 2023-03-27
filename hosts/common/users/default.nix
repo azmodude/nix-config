8bc2f37b89bf0b11f4992f55180d31341475d384
Gordon Schulz <gordon@gordonschulz.de>
2023-03-27 10:58:37 +0200

    Revert "Test specifying users"
    This reverts commit 865f73d716b8017e512a5097bfabfb142dcc256a.

--- hosts/common/users/default.nix
+++ hosts/common/users/default.nix
@@ -1,9 +1,7 @@
-{config, ...}: let
-  userimports = {work-vm = [./j525980 ./root];};
-in {
-  imports =
-    if (builtins.hasAttr config.networking.hostName userimports)
-    then (builtins.getAttr userimports)
-    else [./azmo ./root];
+{
+  imports = [
+    ./azmo
+    ./root
+  ];
   users.mutableUsers = false;
 }
