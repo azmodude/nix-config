865f73d716b8017e512a5097bfabfb142dcc256a
Gordon Schulz <gordon@gordonschulz.de>
2023-03-27 10:46:33 +0200

    Test specifying users

--- hosts/common/users/default.nix
+++ hosts/common/users/default.nix
@@ -1,7 +1,9 @@
-{
-  imports = [
-    ./azmo
-    ./root
-  ];
+{config, ...}: let
+  userimports = {work-vm = [./j525980 ./root];};
+in {
+  imports =
+    if (builtins.hasAttr config.networking.hostName userimports)
+    then (builtins.getAttr userimports)
+    else [./azmo ./root];
   users.mutableUsers = false;
 }
