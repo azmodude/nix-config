7bea125adf72b43bda0bfdaa12bf9b5e27e3b9d2
Gordon Schulz <gordon@gordonschulz.de>
2023-03-27 08:28:31 +0200

    Persist libvirt directories

--- hosts/common/optional/libvirt.nix
+++ hosts/common/optional/libvirt.nix
@@ -1,11 +1,16 @@
-{ config, pkgs, lib, desktop, ...}:
 {
+  config,
+  pkgs,
+  lib,
+  desktop,
+  ...
+}: {
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
       swtpm.enable = true;
       ovmf.enable = true;
-      ovmf.packages = with pkgs; [ OVMFFull.fd ];
+      ovmf.packages = with pkgs; [OVMFFull.fd];
     };
   };
 
@@ -14,17 +19,21 @@
 
   # easier access to most common ovmf files
   environment.etc = {
-  "ovmf/edk2-x86_64-secure-code.fd" = {
-    source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
-    mode = "0644";
-    user = "libvirtd";
-  };
+    "ovmf/edk2-x86_64-secure-code.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-x86_64-secure-code.fd";
+      mode = "0644";
+      user = "libvirtd";
+    };
 
-  "ovmf/edk2-i386-vars.fd" = {
-    source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
-    mode = "0644";
-    user = "libvirtd";
+    "ovmf/edk2-i386-vars.fd" = {
+      source = config.virtualisation.libvirtd.qemu.package + "/share/qemu/edk2-i386-vars.fd";
+      mode = "0644";
+      user = "libvirtd";
+    };
   };
-};
-
+  environment.persistence = {
+    "/persist".directories = [
+      "/var/lib/libvirt"
+    ];
+  };
 }
