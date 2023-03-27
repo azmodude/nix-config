60876574bb1ded83da53eeee38af19038371a373
Gordon Schulz <gordon@gordonschulz.de>
2023-03-27 10:29:07 +0200

    Reorganize scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,7 +7,7 @@ if [ -n "${1}" ]; then
   TARGET_HOST="${1}"
 else
   echo "ERROR! $(basename "${0}") requires a host name:"
-  ls -1 host/ | grep -v ".nix" | grep -v _
+  ls -1 hosts/ | grep -v ".nix" | grep -v common
   exit 1
 fi
 
@@ -23,7 +23,7 @@ if [ -n "${2}" ]; then
   TARGET_USER="${2}"
 else
   echo "ERROR! $(basename "${0}") requires a user name"
-  ls -1 hosts/common/users | grep -v root
+  ls -1 hosts/common/users | grep -v ".nix" | grep -v root
   exit 1
 fi
 
@@ -35,14 +35,19 @@ case "${TARGET_USER}" in
     ;;
 esac
 
-if [ ! -e "host/${TARGET_HOST}/disks.nix" ]; then
-  echo "ERROR! $(basename "${0}") could not find the required host/${TARGET_HOST}/disks.nix"
+if [ ! -e "hosts/${TARGET_HOST}/disks.nix" ]; then
+  echo "ERROR! $(basename "${0}") could not find the required hosts/${TARGET_HOST}/disks.nix"
   exit 1
 fi
 
 if [ "$(id -u)" -eq 0 ]; then
   echo "ERROR! $(basename "${0}") should be run as a regular user"
   exit 1
+fi
+
+if [[ ! -e "/persist/etc/ssh/ssh_host_ed25519_key" ]];
+  echo "ERROR! No ssh_host_ed25519_key found in /persist/etc/ssh."
+  echo "ERROR! Ensure sops-nix configuration for host is correct."
 fi
 
 echo "WARNING! The disks in ${TARGET_HOST} are about to get wiped"
@@ -54,11 +59,9 @@ echo
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     sudo true
 
-    sudo nix run github:nix-community/disko --extra-experimental-features 'nix-command flakes' --no-write-lock-file -- --mode zap_create_mount "host/${TARGET_HOST}/disks.nix"
     sudo nixos-install --no-root-password --flake ".#${TARGET_HOST}"
 
     # Rsync my nix-config to the target install
     mkdir -p "/mnt/home/${TARGET_USER}/nix-config"
     rsync -a --delete "${PWD}/" "/mnt/home/${TARGET_USER}/nix-config/"
-
 fi
