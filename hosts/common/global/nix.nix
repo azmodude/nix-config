33b4a215943dc3e2872a0bfd561eb7d58b6ca479
Gordon Schulz <gordon@gordonschulz.de>
2023-03-27 16:33:24 +0200

    Seperate nix configuration

---
+++ hosts/common/global/nix.nix
@@ -0,0 +1,26 @@
+{ pkgs, inputs, lib, config, ... }:
+{
+  nix = {
+    settings = {
+      trusted-users = [ "root" "@wheel" ];
+      auto-optimise-store = lib.mkDefault true;
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      warn-dirty = false;
+      system-features = [ "kvm" "big-parallel" ];
+    };
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      # Delete older generations too
+      options = "--delete-older-than 7d";
+    };
+
+    # Add each flake input as a registry
+    # To make nix3 commands consistent with the flake
+    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
+
+    # Map registries to channels
+    # Very useful when using legacy commands
+    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
+  };
+}
