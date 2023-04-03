6ab3a81baa94652216fd3fedc9ab5f44762178ec
Gordon Schulz <gordon@gordonschulz.de>
2023-04-03 07:14:44 +0200

    Remove i686-linux

--- flake.nix
+++ flake.nix
@@ -42,7 +42,6 @@
     inherit (self) outputs;
     forAllSystems = nixpkgs.lib.genAttrs [
       "aarch64-linux"
-      "i686-linux"
       "x86_64-linux"
       "aarch64-darwin"
       "x86_64-darwin"
