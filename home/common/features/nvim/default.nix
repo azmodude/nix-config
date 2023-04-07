3e92d02416a905621167354e4b88c9f95a57f12e
Gordon Schulz <gordon@gordonschulz.de>
2023-04-07 09:41:19 +0200

    Update extraPackages for lsp/null-ls requirements
    This will never work with my astronvim configuration on anything
    but nix(os), so we might change this later to actually use Mason

--- home/common/features/nvim/default.nix
+++ home/common/features/nvim/default.nix
@@ -2,7 +2,40 @@
   programs.neovim = {
     enable = true;
     #package = pkgs.neovim-nightly;
-    extraPackages = with pkgs; [ripgrep nodejs gcc fzf python3 gdu bottom tree-sitter];
+    extraPackages = with pkgs; [
+      # astronvim
+      ripgrep
+      nodejs
+      gcc
+      fzf
+      python3
+      gdu
+      bottom
+      tree-sitter
+      # python
+      mypy
+      pylint
+      black
+      isort
+      pyright
+      # docker
+      nodePackages.dockerfile-language-server-nodejs
+      hadolint
+      # shell
+      shellcheck
+      shfmt
+      nodePackages.bash-language-server
+      # lua
+      lua-language-server
+      stylua
+      selene
+      # go
+      gopls
+      gofumpt
+      gotools
+      # markdown
+      marksman
+    ];
     defaultEditor = true;
     vimAlias = true;
     viAlias = true;
@@ -14,8 +47,8 @@
     userConfig = pkgs.fetchFromGitHub {
       owner = "azmodude";
       repo = "astronvim";
-      rev = "2346e5b";
-      sha256 = "sha256-wQH0XnsM6rbPZjId8f8hY3cg6lyAdJJQfAQ9eRlhENk=";
+      rev = "174019b";
+      sha256 = "sha256-PV/PeVmqHSBLvqBz0fPrVbyeiI4+Wk7F2gwaNo2VGp4=";
     };
   };
 }
