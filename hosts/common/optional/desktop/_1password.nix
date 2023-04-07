cfb4ed8496c19d5d1b38000126ddbe6651130197
Gordon Schulz <gordon@gordonschulz.de>
2023-04-07 09:34:45 +0200

    Add optional 1_1password-gui-beta package

--- hosts/common/optional/desktop/_1password.nix
+++ hosts/common/optional/desktop/_1password.nix
@@ -1,7 +1,8 @@
-{
+{pkgs, ...}: {
   programs._1password.enable = true;
   programs._1password-gui = {
     enable = true;
     polkitPolicyOwners = ["azmo"];
+    # package = pkgs._1password-gui-beta;
   };
 }
