e835de9b317491e0198fedc4c9e8ca0344aaf39c
Gordon Schulz <gordon@gordonschulz.de>
2023-04-12 18:11:32 +0200

    feat: Update settings for podman
    - enable dns plugin
    - add docker-compose and podman-compose

--- hosts/common/global/containers.nix
+++ hosts/common/global/containers.nix
@@ -7,13 +7,17 @@
     enable = true;
     dockerCompat = true;
     dockerSocket.enable = true;
+    defaultNetwork.settings = {
+      dns_enabled = true;
+    };
   };
+  # add dnsname plugin for cni
+  virtualisation.containers.containersConf.cniPlugins = with pkgs; [cni-plugins dnsname-cni];
+  environment.systemPackages = with pkgs; [distrobox docker-compose podman-compose];
 
   environment.persistence = {
     "/persist".directories = [
       "/var/lib/containers"
     ];
   };
-
-  environment.systemPackages = [pkgs.distrobox];
 }
