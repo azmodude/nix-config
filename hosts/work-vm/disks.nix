b5f14a7b55268d65c776650c58647271a1d77239
Gordon Schulz <gordon@gordonschulz.de>
2023-04-15 12:29:02 +0200

    fix: wrap device declaration in disko.devices
    Introduced by https://github.com/nix-community/disko/pull/162

--- hosts/work-vm/disks.nix
+++ hosts/work-vm/disks.nix
@@ -3,45 +3,47 @@
   swapsize ? "4G",
   ...
 }: {
-  disk = {
-    disk0 = {
-      type = "disk";
-      device = builtins.elemAt disks 0;
-      content = {
-        type = "table";
-        format = "gpt";
-        partitions = [
-          {
-            type = "partition";
-            name = "ESP";
-            start = "1MiB";
-            end = "100MiB";
-            bootable = true;
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
-              mountOptions = [
-                "defaults"
-              ];
-            };
-          }
-          {
-            type = "partition";
-            name = "luks";
-            start = "100MiB";
-            end = "100%";
-            content = {
-              type = "luks";
-              name = "crypt-system";
-              extraOpenArgs = ["--allow-discards"];
+  disko.devices = {
+    disk = {
+      disk0 = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              type = "partition";
+              name = "ESP";
+              start = "1MiB";
+              end = "100MiB";
+              bootable = true;
               content = {
-                type = "lvm_pv";
-                vg = "lvm-crypt-system";
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
               };
-            };
-          }
-        ];
+            }
+            {
+              type = "partition";
+              name = "luks";
+              start = "100MiB";
+              end = "100%";
+              content = {
+                type = "luks";
+                name = "crypt-system";
+                extraOpenArgs = ["--allow-discards"];
+                content = {
+                  type = "lvm_pv";
+                  vg = "lvm-crypt-system";
+                };
+              };
+            }
+          ];
+        };
       };
     };
   };
