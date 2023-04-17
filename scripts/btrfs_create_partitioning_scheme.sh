4c1322610566785bd697d8cadb3bde6fe9cc286b
Gordon Schulz <gordon@gordonschulz.de>
2023-04-17 18:40:17 +0200

    chore: Remove unused subvolume

--- scripts/btrfs_create_partitioning_scheme.sh
+++ scripts/btrfs_create_partitioning_scheme.sh
@@ -41,7 +41,6 @@ btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
 btrfs subvolume create /mnt/@nix
 btrfs subvolume create /mnt/@persist
-btrfs subvolume create /mnt/@persist/games
 btrfs subvolume create /mnt/@snapshots
 btrfs subvolume create /mnt/@var-log
 
