b4e206398e534454e4d3e1b661c0d58cb0178b36
Gordon Schulz <gordon@gordonschulz.de>
2023-08-20 14:04:51 +0200

    feat: Mount and create needed games subvolumes

---
+++ hosts/artemis/games.nix
@@ -0,0 +1,19 @@
+{ user, ...
+
+}:
+{
+
+  fileSystems."/persist/games" = {
+      device = "/dev/disk/by-label/btrfs-root";
+      fsType = "btrfs";
+      options = ["subvol=data/@persist/games" "compress=zstd"];
+    };
+
+  # ensure persist game directory exists and has correct permission
+  systemd.tmpfiles.rules = [
+    "d /persist/games 0755 ${user} ${user} - -"
+    "z /persist/games 0755 ${user} ${user} - -"
+  ];
+
+
+}
