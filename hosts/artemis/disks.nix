99058cfcf93c4a5fcd40cae9c32e3b7f424a55e3
Gordon Schulz <gordon@gordonschulz.de>
2023-08-20 09:21:02 +0200

    feat: Add artemis disks.nix

---
+++ hosts/artemis/disks.nix
@@ -0,0 +1,101 @@
+{
+  disks ? ["/dev/nvme0n1"],
+  swapsize ? "16G",
+  ...
+}: {
+  disko.devices = {
+    disk = {
+      disk0 = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            {
+              name = "ESP";
+              start = "1MiB";
+              end = "100MiB";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                extraArgs = ["-n ESP"];
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            }
+            {
+              name = "luks";
+              start = "100MiB";
+              end = "100%";
+              content = {
+                type = "luks";
+                name = "crypt-system";
+                extraFormatArgs = ["--label crypt-system"];
+                extraOpenArgs = ["--allow-discards"];
+                content = {
+                  type = "lvm_pv";
+                  vg = "lvm-crypt-system";
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
+    lvm_vg = {
+      lvm-crypt-system = {
+        type = "lvm_vg";
+        lvs = {
+          a-swap = {
+            size = swapsize;
+            name = "swap";
+            content = {
+              type = "swap";
+            };
+          };
+          b-root = {
+            size = "100%FREE";
+            name = "root";
+            content = {
+              type = "btrfs";
+              extraArgs = ["-f" "--label btrfs-root"];
+              mountpoint = "/btrfs-root";
+              subvolumes = {
+                "system" = {};
+                "system/@" = {
+                  mountpoint = "/";
+                  mountOptions = ["defaults" "compress=zstd" "noatime"];
+                };
+                "system/@snapshots" = {
+                  mountpoint = "/btrfs-root/snapshots";
+                  mountOptions = ["defaults" "compress=zstd" "noatime"];
+                };
+                "system/@nix" = {
+                  mountpoint = "/nix";
+                  mountOptions = ["defaults" "compress=zstd" "noatime"];
+                };
+                "system/@var-log" = {
+                  mountpoint = "/var/log";
+                  mountOptions = ["defaults" "compress=zstd" "noatime"];
+                };
+                "data" = {};
+                "data/@home" = {
+                  mountpoint = "/home";
+                  mountOptions = ["defaults" "compress=zstd" "relatime"];
+                };
+                "data/@persist" = {
+                  mountpoint = "/persist";
+                  mountOptions = ["defaults" "compress=zstd" "noatime"];
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
