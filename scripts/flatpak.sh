93f90300b61db16cf615341443f54c7985a27cc4
Gordon Schulz <gordon@gordonschulz.de>
2023-08-23 07:54:04 +0200

    feat: Add more flatpak gaming features

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -28,5 +28,19 @@ artemis)
 	flatpak install --user -y com.usebottles.bottles
 	flatpak install --user -y com.valvesoftware.Steam.Utility.steamtinkerlaunch
 	flatpak install --user -y net.davidotek.pupgui2
+	flatpak install --user -y org.freedesktop.Platform.VulkanLayer.gamescope
+	flatpak install --user -y org.freedesktop.Platform.VulkanLayer.MangoHud//22.08
+	flatpak install --user -y org.freedesktop.Platform.VulkanLayer.OBSVkCapture//22.08
+	flatpak install --user -y org.freedesktop.Platform.VulkanLayer.vkBasalt//22.08
+	flatpak install --user -y com.heroicgameslauncher.hgl
+
+	# allow bottles to create desktop links
+	flatpak override com.usebottles.bottles --user --filesystem=xdg-data/applications
+
+	# configure MangoHud
+	for flatpak in com.valvesoftware.Steam com.heroicgameslauncher.hgl com.usebottles.bottles net.lutris.Lutris; do
+		flatpak override --user --env=MANGOHUD=1 ${flatpak}
+		flatpak override --user --filesystem=xdg-config/MangoHud:ro ${flatpak}
+	done
 	;;
 esac
