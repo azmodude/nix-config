5cb40ca5bc0f151194a6e6b0b4adb4878ebb4219
Gordon Schulz <gordon@gordonschulz.de>
2023-09-03 08:29:35 +0200

    feat(exa): Add extraOptions and aliases for newest/oldest files sorting

--- home/common/features/cli/exa.nix
+++ home/common/features/cli/exa.nix
@@ -2,8 +2,18 @@
   programs.exa = {
     enable = true;
     enableAliases = true;
+    extraOptions = [
+      "--group-directories-first"
+      "--group"
+    ];
     git = true;
     icons = true;
   };
   home.sessionVariables.EXA_ICON_SPACING = 2;
+  home.shellAliases = {
+    # list newest first
+    lln = "exa -la --sort modified --reverse";
+    # list oldest first
+    llo = "exa -la --sort modified";
+  };
 }
