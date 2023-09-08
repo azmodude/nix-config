27378c4e48c24cc8658942fc3794634b5b7ede4f
Gordon Schulz <gordon@gordonschulz.de>
2023-09-08 08:58:22 +0200

    feat: Add HP Officejet 8730 as default printer

--- hosts/common/optional/desktop/cups.nix
+++ hosts/common/optional/desktop/cups.nix
@@ -1,4 +1,8 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   services.printing = {
     enable = true;
     startWhenNeeded = true;
@@ -8,5 +12,33 @@
     enable = true;
     nssmdns = true;
     openFirewall = true;
+  };
+
+  hardware = let
+    hp = "HP_Officejet_8730_Postscript";
+    hpHostName = "officejet8730.fritz.box";
+  in {
+    printers = {
+      # for color to monochrome conversion to work, use:
+      # lpadmin -p HP_Officejet_8730_Postscript -o pdftops-renderer-default=gs
+      # (and probably turn off HPPJLColorAsGray option)
+      ensureDefaultPrinter = hp;
+      ensurePrinters = [
+        {
+          name = hp;
+          deviceUri = "socket://${hpHostName}:9100";
+          # deviceUri = "ipp://${hpHostName}/ipp/print";
+          model = "HP/hp-officejet_pro_8730-ps.ppd.gz";
+          description = lib.replaceStrings ["_"] [" "] hp;
+          location = "Home Office";
+          ppdOptions = {
+            PageSize = "A4";
+            Collate = "true";
+            MediaType = "Plain";
+            HPPJLColorAsGray = "yes";
+          };
+        }
+      ];
+    };
   };
 }
