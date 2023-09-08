ac758bb25ee45f4e599094b5165a6354d1326b03
Gordon Schulz <gordon@gordonschulz.de>
2023-09-08 09:20:15 +0200

    feat: Add xcursor size and theme detection from Gnome

--- home/common/features/desktop/common/wezterm.nix
+++ home/common/features/desktop/common/wezterm.nix
@@ -10,6 +10,22 @@
        end
       end
 
+      -- set xcursor size and theme from gnome
+      -- source: https://github.com/wez/wezterm/issues/1742#issuecomment-1075333507
+      -- could possibly be removed when wezterm gets full wayland support
+      local xcursor_size = nil
+      local xcursor_theme = nil
+
+      local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-theme"})
+      if success then
+       xcursor_theme = stdout:gsub("'(.+)'\n", "%1")
+      end
+
+      local success, stdout, stderr = wezterm.run_child_process({"gsettings", "get", "org.gnome.desktop.interface", "cursor-size"})
+      if success then
+       xcursor_size = tonumber(stdout)
+      end
+
       return {
        -- ...your existing config
        color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
@@ -20,7 +36,8 @@
         }),
         font_size = 11,
         hide_tab_bar_if_only_one_tab = true,
-        xcursor_theme = "Adwaita",
+        xcursor_theme = xcursor_theme,
+        xcursor_size = xcursor_size,
         window_frame = {
          font = wezterm.font { family = "Inter", weight = "Bold" },
          font_size = 10.0,
