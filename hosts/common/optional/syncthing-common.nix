21b16a5a443cf234fa533bd1c7c184ec60e69ec3
Gordon Schulz <gordon@gordonschulz.de>
2023-09-28 17:06:37 +0200

    feat: add options to syncthing

--- hosts/common/optional/syncthing-common.nix
+++ hosts/common/optional/syncthing-common.nix
@@ -11,6 +11,14 @@
     user = user.name;
     cert = config.sops.secrets."syncthing/cert".path;
     key = config.sops.secrets."syncthing/key".path;
+    settings = {
+      gui = {
+        user = "syncthing";
+      };
+      options = {
+        urAccepted = 1;
+      };
+    };
   };
   sops.secrets."syncthing/cert" = {
     sopsFile = ../../${config.networking.hostName}/secrets.yaml;
