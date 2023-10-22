3e18f66dc668d4884d2a14e573952ffd20c0f3d4
Gordon Schulz <gordon@gordonschulz.de>
2023-10-22 13:21:52 +0200

    feat: Implement restic backup for WoW Interface and WTF

--- home/azmo/hosts/artemis.nix
+++ home/azmo/hosts/artemis.nix
@@ -18,4 +18,39 @@
     ]
     ++ lib.optional (builtins.isString desktop.environment) ../../common/features/desktop/common
     ++ lib.optional (builtins.isString desktop.environment) ../../common/features/desktop/${desktop.environment};
+
+  systemd.user.services = {
+    backup-wow = {
+      Unit = {
+        Description = "rsync wow Interface and WTF to a place restic will pick it up";
+        RequiresMountFor = "/home";
+      };
+      Service = {
+        Type = "oneshot";
+        ExecStartPre = "${pkgs.coreutils}/bin/mkdir -p %h/Games/WoW";
+        ExecStart = pkgs.writeShellScript "backup-wow.sh" ''
+          ${pkgs.rsync}/bin/rsync -av "$HOME/.var/app/com.usebottles.bottles/data/bottles/bottles/WoW/drive_c/Program Files (x86)/World of Warcraft/_retail_/Interface" "$HOME/Games/WoW"
+          ${pkgs.rsync}/bin/rsync -av "$HOME/.var/app/com.usebottles.bottles/data/bottles/bottles/WoW/drive_c/Program Files (x86)/World of Warcraft/_retail_/WTF" "$HOME/Games/WoW"
+        '';
+      };
+      Install = {
+        WantedBy = ["multi-user.target"];
+      };
+    };
+  };
+  systemd.user.timers = {
+    backup-wow = {
+      Unit = {
+        Description = "rsync wow Interface and WTF to a place restic will pick it up once per hour";
+      };
+      Timer = {
+        Unit = "backup-wow";
+        OnBootSec = "30m";
+        OnCalendar = "hourly";
+      };
+      Install = {
+        WantedBy = ["timers.target"];
+      };
+    };
+  };
 }
