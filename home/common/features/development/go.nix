a517d77167adbae22cc165f714aa4039fe013fb8
Gordon Schulz <gordon@gordonschulz.de>
2023-11-06 08:10:41 +0100

    chore: Update golang tools

--- home/common/features/development/go.nix
+++ home/common/features/development/go.nix
@@ -1,3 +1,4 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [go gopls jetbrains.goland gofumpt gotools golines delve];
+  # staticcheck is in gotools
+  home.packages = with pkgs; [go gopls jetbrains.goland gofumpt gotools golines revive delve impl gomodifytags golangci-lint];
 }
