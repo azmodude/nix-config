5ac915baa36630d5cbed23e750f60b8020c7ac0f
Gordon Schulz <gordon@gordonschulz.de>
2024-01-29 19:50:57 +0100

    chore(ssh): Add cloud-general key and normalize paths

--- home/azmo/features/ssh.nix
+++ home/azmo/features/ssh.nix
@@ -1,8 +1,9 @@
 {
   # write SSH public keys
-  home.file.".ssh/general.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUkEk7GV/qWMR9SJFYSJSxwnPxR8fG2Fn9VILHcyPYQ";
-  home.file.".ssh/git.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQh4341SBJaXdFAvDtAvS9zs4tiS5v8jod0OhgzBXpV";
-  home.file.".ssh/netcup.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGgxP6hdnBAK8gYgHBFwmtNvgQmFii0RUiSifZ3KgmlU";
-  home.file.".ssh/upcloud.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ6nxegS8y2rC6ldf4mxG3iyuJilxvDLzCote2RSmViG";
-  home.file.".ssh/luks-unlock-workstations.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAFOj2KxBYTb6MclwoN4xgKN7OgX4Iwv4lViluKPdL9K";
+  home.file.".ssh/keys.public/general.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMUkEk7GV/qWMR9SJFYSJSxwnPxR8fG2Fn9VILHcyPYQ";
+  home.file.".ssh/keys.public/cloud-general.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP6UF18GhwVcmv4UUqcxBcu9muWUiKbRqT5yaQ6BmQ2j";
+  home.file.".ssh/keys.public/git.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICQh4341SBJaXdFAvDtAvS9zs4tiS5v8jod0OhgzBXpV";
+  home.file.".ssh/keys.public/netcup.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGgxP6hdnBAK8gYgHBFwmtNvgQmFii0RUiSifZ3KgmlU";
+  home.file.".ssh/keys.public/upcloud.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ6nxegS8y2rC6ldf4mxG3iyuJilxvDLzCote2RSmViG";
+  home.file.".ssh/keys.public/luks-unlock-workstations.pub".text = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAFOj2KxBYTb6MclwoN4xgKN7OgX4Iwv4lViluKPdL9K";
 }
