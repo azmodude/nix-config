2660ec0d0ee960e8404d41822f9523d8813b12e3
Gordon Schulz <gordon@gordonschulz.de>
2024-02-04 12:35:03 +0100

    feat(incus): add preseed

--- hosts/common/optional/incus.nix
+++ hosts/common/optional/incus.nix
@@ -2,6 +2,52 @@
   virtualisation.incus = {
     enable = true;
     socketActivation = true;
+    preseed = {
+      config = {};
+      networks = [
+        {
+          config = {
+            ipv4.address = "auto";
+            ipv6.address = "auto";
+          };
+          description = "Default network";
+          name = "incusbr0";
+          type = "";
+          project = "default";
+        }
+      ];
+      storage_pools = [
+        {
+          config = {
+            source = "/var/lib/incus/storage-pools/default";
+          };
+          description = "Default storage pool";
+          name = "default";
+          driver = "btrfs";
+        }
+      ];
+      profiles = [
+        {
+          config = {};
+          description = "Default profile";
+          devices = {
+            eth0 = {
+              name = "eth0";
+              network = "incusbr0";
+              type = "nic";
+            };
+            root = {
+              path = "/";
+              pool = "default";
+              type = "disk";
+            };
+          };
+          name = "default";
+        }
+      ];
+      projects = [];
+      cluster = null;
+    };
   };
   # Trust incus bridging interface
   networking.firewall.trustedInterfaces = ["incusbr0"];
