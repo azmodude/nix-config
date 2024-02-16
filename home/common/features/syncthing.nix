e6f2bd5e76ca4baf4f1fa1ee63f20c45f3dfdeca
Gordon Schulz <gordon@gordonschulz.de>
2024-02-16 20:13:45 +0100

    chore(syncthing): Update paths

--- home/common/features/syncthing.nix
+++ home/common/features/syncthing.nix
@@ -6,9 +6,9 @@
 }: {
   # make syncthing related links for workstations
   systemd.user.tmpfiles.rules = [
-    "L+ /persist/home/${user.name}/Documents/synced - - - - /persist/home/${user.name}/sync/workstations/documents"
-    "L+ /persist/home/${user.name}/Pictures/synced - - - - /persist/home/${user.name}/sync/workstations/pictures"
-    "L+ /persist/home/${user.name}/projects/synced - - - - /persist/home/${user.name}/sync/workstations/projects"
+    "L+ /persist/home/${user.name}/Documents/sync - - - - /persist/home/${user.name}/sync/workstations-documents"
+    "L+ /persist/home/${user.name}/Pictures/sync - - - - /persist/home/${user.name}/sync/workstations-pictures"
+    "L+ /persist/home/${user.name}/projects/sync - - - - /persist/home/${user.name}/sync/workstations-projects"
   ];
   home.packages = with pkgs; [syncthingtray];
 }
