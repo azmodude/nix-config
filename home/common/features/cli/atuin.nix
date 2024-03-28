ef9ba340896ffdaef4fc542da79494851c04f6e2
Gordon Schulz <gordon@gordonschulz.de>
2024-03-28 11:11:48 +0100

    feat(atuin): Bring config uptodate

--- home/common/features/cli/atuin.nix
+++ home/common/features/cli/atuin.nix
@@ -1,16 +1,22 @@
 {config, ...}: {
   programs.atuin = {
     enable = true;
+    enableZshIntegration = true;
     settings = {
       auto_sync = true;
       enter_accept = true;
       sync_frequency = "5m";
+      keymap_mode = "vim-insert";
       filter_mode = "global";
       filter_mode_shell_up_key_binding = "host";
       inline_height = "40";
       update_check = "false";
+      show_help = "true";
       show_preview = "true";
       workspaces = "true";
+      sync = {
+        records = "true";
+      };
     };
   };
   sops = {
