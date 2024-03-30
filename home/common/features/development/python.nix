8bc4b4868c5f44d361d1d51a7f52304a8d6e3800
Gordon Schulz <gordon@gordonschulz.de>
2024-03-30 08:31:30 +0100

    feat(development): Use nix way of adding plugins to jetbrains IDEs

--- home/common/features/development/python.nix
+++ home/common/features/development/python.nix
@@ -1,3 +1,14 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [mypy pylint black isort pyright jetbrains.pycharm-professional];
+  home.packages = with pkgs; [
+    mypy
+    pylint
+    black
+    isort
+    pyright
+    (jetbrains.plugins.addPlugins pkgs.jetbrains.pycharm-professional [
+      "github-copilot"
+      "ideavim"
+      "nixidea"
+    ])
+  ];
 }
