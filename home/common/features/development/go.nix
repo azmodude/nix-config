979f8f81d39ebea7691e2055ebffb0d32598228b
Gordon Schulz <gordon@gordonschulz.de>
2024-03-30 08:31:30 +0100

    feat(development): Use nix way of adding plugins to jetbrains IDEs

--- home/common/features/development/go.nix
+++ home/common/features/development/go.nix
@@ -1,4 +1,20 @@
 {pkgs, ...}: {
   # staticcheck is in gotools
-  home.packages = with pkgs; [go jetbrains.goland gopls gofumpt gotools golines revive delve impl gomodifytags golangci-lint];
+  home.packages = with pkgs; [
+    go
+    gopls
+    gofumpt
+    gotools
+    golines
+    revive
+    delve
+    impl
+    gomodifytags
+    golangci-lint
+    (jetbrains.plugins.addPlugins pkgs.jetbrains.goland [
+      "github-copilot"
+      "ideavim"
+      "nixidea"
+    ])
+  ];
 }
