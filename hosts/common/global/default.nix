29e941b243b7ab4bd861c61f79c04cd73c3d5356
Gordon Schulz <gordon@gordonschulz.de>
2024-06-12 14:55:42 +0200

    fix: Enable spice-vdagentd for every host

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -128,6 +128,14 @@
       }
     ];
   };
+
+  services = {
+    spice-vdagentd = {
+      # enable spice-vdagent globally
+      enable = true;
+    };
+  };
+
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
