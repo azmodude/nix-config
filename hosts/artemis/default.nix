ea39d8694e952acb97cae8a6b8d68332c35ad456
Gordon Schulz <gordon@gordonschulz.de>
2024-06-15 12:35:53 +0200

    chore: update flakes

--- hosts/artemis/default.nix
+++ hosts/artemis/default.nix
@@ -44,6 +44,7 @@
       ../common/optional/desktop/logitech.nix
       ../common/optional/syncthing-common.nix
       ../common/optional/syncthing-workstations.nix
+      ../common/optional/networkmanager.nix
 
       # Only include desktop components if one is supplied.
       # - https://nixos.wiki/wiki/Nix_Language:_Tips_%26_Tricks#Coercing_a_relative_path_with_interpolated_variables_to_an_absolute_path_.28for_imports.29
