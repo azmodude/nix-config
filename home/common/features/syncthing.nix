c353fde0fb1cac517ab14ac5adf12b8b713d41e1
Gordon Schulz <gordon@gordonschulz.de>
2024-06-15 13:01:51 +0200

    feat: Remove default ~/Sync link created by syncthing

--- home/common/features/syncthing.nix
+++ home/common/features/syncthing.nix
@@ -9,5 +9,6 @@
     "L+ /persist/home/${user.name}/Documents/sync - - - - /persist/home/${user.name}/sync/workstations-documents"
     "L+ /persist/home/${user.name}/Pictures/sync - - - - /persist/home/${user.name}/sync/workstations-pictures"
     "L+ /persist/home/${user.name}/projects/sync - - - - /persist/home/${user.name}/sync/workstations-projects"
+    "R  /home/${user.name}/Sync                  - - - - -"
   ];
 }
