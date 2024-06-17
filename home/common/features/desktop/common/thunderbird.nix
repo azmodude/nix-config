7b54901db6bdb32bbb014b445a792aa25d561c1b
Gordon Schulz <gordon@gordonschulz.de>
2024-06-17 19:33:23 +0200

    feat: persist .thunderbird

--- home/common/features/desktop/common/thunderbird.nix
+++ home/common/features/desktop/common/thunderbird.nix
@@ -1,9 +1,18 @@
-{
+{user, ...}: {
   programs.thunderbird = {
     enable = true;
     profiles = {
       azmo-default = {
         isDefault = true;
+      };
+    };
+  };
+  home = {
+    persistence = {
+      "/persist/home/${user.name}" = {
+        directories = [
+          ".thunderbird"
+        ];
       };
     };
   };
